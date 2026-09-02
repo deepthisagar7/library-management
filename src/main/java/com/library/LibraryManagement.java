bc5d90cdd177ce2cdb022bad1f5cebaa265aa09d
warun <warunsagar7@gmail.com>
2026-09-02 15:45:56 +0530

    Initial commit

---
+++ src/main/java/com/library/LibraryManagement.java
@@ -0,0 +1,15 @@
+package com.library;
+
+public class LibraryManagement {
+
+    public static void main(String[] args) {
+
+        System.out.println("Library Management System");
+
+        System.out.println("1. Search Book");
+        System.out.println("2. Issue Book");
+        System.out.println("3. Return Book");
+        System.out.println("4. Reserve Book");
+
+    }
+}
